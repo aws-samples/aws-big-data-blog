01d9f47cc8242a800dcd438fb2eeaa7123d46b02
IanMeyers <meyersi@amazon.com>
2015-10-12 04:33:53 -0700

    Updated Mapper and Reducer Samples

--- aws-blog-nodejs-on-emr/src/sample-reducer.js
+++ aws-blog-nodejs-on-emr/src/sample-reducer.js
@@ -1,90 +1,43 @@
-#!/usr/bin/env node
+// use RL to read lines from stdin
+var readline = require('readline');
+var rl = readline.createInterface({
+	input : process.stdin
+});
 
-var events = require('events');
-var emitter = new events.EventEmitter();
-
-var remaining = '';
-var lineReady = 'lineReady';
-var dataReady = 'dataReady';
-
+// variable used as a daily accumulator
 var interactionSummary = {
 	day : '',
 	count : 0
 };
 
-// escape all control characters so that they are plain text in the output
-String.prototype.escape = function() {
-	return this.replace('\n', '\\n').replace('\'', '\\\'').replace('\"', '\\"')
-			.replace('\&', '\\&').replace('\r', '\\r').replace('\t', '\\t')
-			.replace('\b', '\\b').replace('\f', '\\f');
-}
-
-// append an array to this one
-Array.prototype.appendArray = function(arr) {
-	this.push.apply(this, arr);
-}
-
-// data is complete, write it to the required output channel
-emitter.on(dataReady, function(o) {
-	if (o) {
-		process.stdout.write(JSON.stringify(o) + '\n');
-	}
-});
-
 // generate a JSON object from the captured input data, and then generate
 // the required output
-emitter.on(lineReady,function(data) {	
-	if (!data || data == '') {
-		// null data is probably a closing event, so emit a data ready
-		emitter.emit(dataReady, interactionSummary);
-		return;
-	}
-	
-	try {
-		obj = JSON.parse(data.split('\t')[1]);
-	} catch (err) {
-		process.stderr.write('Error Processing Line ' + data + '\n')
-		process.stderr.write(err);
-		return;
-	}
+exports.processMapData = function(data) {
+	lineElements = data.split('\t')
+	var keyDate = lineElements[0];
+	obj = JSON.parse(lineElements[1]);
 
-	if (interactionSummary.day == '') {
-		interactionSummary.day = obj.key_date;
-		interactionSummary.count = 1;		
+	if (interactionSummary.day === '') {
+		interactionSummary.day = keyDate;
+		interactionSummary.count = 1;
 	} else {
-		if (obj.key_date != interactionSummary.day) {
-			// raise an event that the reduced array is completed
-			emitter.emit(dataReady, interactionSummary);
-			interactionSummary.day = obj.key_date;
+		if (keyDate !== interactionSummary.day) {
+			process.stdout.write(JSON.stringify(interactionSummary) + '\n');
+
+			interactionSummary.day = keyDate;
 			interactionSummary.count = 1;
 		} else {
 			interactionSummary.count += 1;
 		}
 	}
+};
+
+// fire an event on each line read from RL
+rl.on('line', function(line) {
+	exports.processMapData(line);
 });
 
-// fires on every block of data read from stdin
-process.stdin.on('data', function(chunk) {
-	var capture = chunk.split('\n');
-
-	for (var i=0;i<capture.length-1; i++) {
-		if (i==0) {
-			emitter.emit(lineReady,remaining + capture[i]);
-		} else if (i<capture.length-1) {
-			emitter.emit(lineReady,capture[i]);
-		} else {
-			remaining = capture[i];
-		}
-	}
-});
-
-// fires when stdin is completed being read
-process.stdin.on('end', function() {
-	emitter.emit(lineReady,remaining);
-});
-
-// resume STDIN - paused by default
-process.stdin.resume();
-
-// set up the encoding for STDIN
-process.stdin.setEncoding('utf8');
+// final event when the file is closed, to flush the final accumulated value
+rl.on('close', function(line) {
+	process.stdout.write(JSON.stringify(interactionSummary) + '\n');
+})
