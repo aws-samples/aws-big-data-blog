4665c5fe80cfc14e07ac034e32cf65c603d31009
Hernan Vivani <hernan@vivani.com.ar>
2016-06-25 17:16:29 +0000

    adding install script

--- aws-blog-bigtop-application-emr/puppet_install_elasticsearch.sh
+++ aws-blog-bigtop-application-emr/puppet_install_elasticsearch.sh
@@ -2,7 +2,7 @@
 set -x
 
 echo "creating custom Bigtop repository"
-sudo aws s3 cp s3://<your-bucket>/bigtop/repos/Bigtop_custom.repo  /etc/yum.repos.d/Bigtop_custom.repo
+sudo aws s3 cp s3://vivanih-emr/bigtop/repos/Bigtop_custom.repo  /etc/yum.repos.d/Bigtop_custom.repo
 echo "creating application module and manifest directory"
 sudo mkdir -p /home/hadoop/bigtop/bigtop-deploy/puppet/modules/elasticsearch/manifests/
 echo "changing directory"
@@ -13,8 +13,9 @@ echo "creating application template directory"
 sudo mkdir -p /home/hadoop/bigtop/bigtop-deploy/puppet/modules/elasticsearch/templates/
 echo "changing directory"
 cd /home/hadoop/bigtop/bigtop-deploy/puppet/modules/elasticsearch/templates/
-echo "getting puppet template"
+echo "getting puppet templates"
 sudo wget https://raw.githubusercontent.com/hvivani/aws-big-data-blog/patch-1/aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/templates/elasticsearch.yml
+sudo wget https://raw.githubusercontent.com/hvivani/aws-big-data-blog/patch-1/aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/templates/elasticsearch.conf
 
 echo "running puppet apply"
 sudo puppet apply --verbose -d --modulepath=/home/hadoop/bigtop/bigtop-deploy/puppet/modules:/etc/puppet/modules  -e 'include elasticsearch::client'
