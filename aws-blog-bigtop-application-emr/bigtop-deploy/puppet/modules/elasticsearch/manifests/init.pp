ad3603121c97860aa39bd33a2b9637fd466d1450
Hernan vivani <hernan@vivani.com.ar>
2016-07-06 16:00:32 +0000

    changing puppet script

--- aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/manifests/init.pp
+++ aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/manifests/init.pp
@@ -12,10 +12,12 @@
 
 class elasticsearch {
   class client {
-
+ 
+    # Obtaining configuration parameters from cluster configuration files.
     $clusterId = generate ("/bin/bash", "-c", "cat /mnt/var/lib/instance-controller/extraInstanceData.json |grep \"jobFlowId\" | cut -d':' -f2 | tr -d '\"' |tr -d ',' |tr -d ' '")
     $region = generate ("/bin/bash", "-c", "cat /mnt/var/lib/instance-controller/extraInstanceData.json |grep 'region\"' | cut -d':' -f2 | tr -d '\"' |tr -d ',' |tr -d ' '")
     $isMaster = generate ("/bin/bash", "-c", "cat /mnt/var/lib/info/instance.json | grep \"isMaster\" | cut -d':' -f2 | tr -d '\"' |tr -d ',' |tr -d ' '")
+    # Setting default port configuration
     $elasticsearch_port = '9200'
 
     include common
@@ -24,23 +26,30 @@ class elasticsearch {
 
 
   class common () {
+    # elasticsearch package installation. This line will run a 'yum install' command.
     package { ["elasticsearch"]: ensure => latest, }
 
+    # elasticsearch service configuration from elasticsearch.yml template
     file {
       "/etc/elasticsearch/elasticsearch.yml":
       content => template("elasticsearch/elasticsearch.yml"),
     }
 
+    # elasticsearch aws plugin installation. 
     exec { "install aws plugin":
       command => "/usr/share/elasticsearch/bin/plugin -install elasticsearch/elasticsearch-cloud-aws/2.6.0",
+      onlyif  => "test ! -d /usr/share/elasticsearch/plugins/cloud-aws", 
+      path    => ['/usr/bin', 'usr/sbin', '/bin', '/sbin'],
       require => [Package["elasticsearch"]],
     }
-    
+   
+    # upstart init script installed from puppet template 
     file {
       "/etc/init/elasticsearch.conf":
       content => template("elasticsearch/elasticsearch.conf"),
     }
 
+    # elasticsearch service handling. service will restart on any change on file 'elasticsearch.yml'
     service { "elasticsearch":
       ensure => running,
       require => Package["elasticsearch"],
