39c8d66b7ef213771474c9535c5ec66053322898
Hernan Vivani <hernan@vivani.com.ar>
2016-07-26 14:43:14 -0700

    Update puppet_install_elasticsearch.sh

--- aws-blog-bigtop-application-emr/puppet_install_elasticsearch.sh
+++ aws-blog-bigtop-application-emr/puppet_install_elasticsearch.sh
@@ -8,14 +8,14 @@ sudo mkdir -p /home/hadoop/bigtop/bigtop-deploy/puppet/modules/elasticsearch/man
 echo "changing directory"
 cd /home/hadoop/bigtop/bigtop-deploy/puppet/modules/elasticsearch/manifests/
 echo "getting puppet recipe"
-sudo  wget https://raw.githubusercontent.com/hvivani/aws-big-data-blog/patch-1/aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/manifests/init.pp
+sudo  https://raw.githubusercontent.com/awslabs/aws-big-data-blog/master/aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/manifests/init.pp
 echo "creating application template directory"
 sudo mkdir -p /home/hadoop/bigtop/bigtop-deploy/puppet/modules/elasticsearch/templates/
 echo "changing directory"
 cd /home/hadoop/bigtop/bigtop-deploy/puppet/modules/elasticsearch/templates/
 echo "getting puppet templates"
-sudo wget https://raw.githubusercontent.com/hvivani/aws-big-data-blog/patch-1/aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/templates/elasticsearch.yml
-sudo wget https://raw.githubusercontent.com/hvivani/aws-big-data-blog/patch-1/aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/templates/elasticsearch.conf
+sudo wget https://raw.githubusercontent.com/awslabs/aws-big-data-blog/master/aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/templates/elasticsearch.yml
+sudo wget https://raw.githubusercontent.com/awslabs/aws-big-data-blog/master/aws-blog-bigtop-application-emr/bigtop-deploy/puppet/modules/elasticsearch/templates/elasticsearch.conf
 
 echo "running puppet apply"
 sudo puppet apply --verbose -d --modulepath=/home/hadoop/bigtop/bigtop-deploy/puppet/modules:/etc/puppet/modules  -e 'include elasticsearch::client'
