98851310b14ae54c8b6a91c79800685b292a2e78
Aaron Friedman <ajfriedm@amazon.com>
2016-12-07 10:31:10 -0800

    try/finally

--- aws-blog-athena-genomics/etl/thousand_genomes/create_trimmed_parquet.py
+++ aws-blog-athena-genomics/etl/thousand_genomes/create_trimmed_parquet.py
@@ -37,33 +37,31 @@ def convert_alleles_to_genotypes(record):
 if __name__ == "__main__":
     sc = SparkContext()
 
-    spark = SparkSession.builder.config(conf=SparkConf()).getOrCreate()
-
-    argparser = ArgumentParser()
-
-    argparser.add_argument('--input_s3_path', type=str, help='Input Parquet Path', required=True)
-    argparser.add_argument('--output_s3_path', type=str, help='Output Parquet Path', required=True)
-
-    args = argparser.parse_args()
-
-    df = spark.read.parquet(args.input_s3_path)
-    df = df.repartition(20)
-
-    # Select only the fields that we want to put into Athena
-    athena_df = df.select(df.contigName.alias('chromosome'),
-                          'start',
-                          'end',
-                          df.variant.referenceAllele.alias('ref'),
-                          df.variant.alternateAllele.alias('alt'),
-                          'sampleId',
-                          'alleles')
-
-    athena_df = spark.createDataFrame(athena_df.rdd.flatMap(lambda x: convert_alleles_to_genotypes(x)))
-
-    athena_df.write.mode('overwrite').parquet(args.output_s3_path)
-
     try:
-        sc.stop()
+        spark = SparkSession.builder.config(conf=SparkConf()).getOrCreate()
+
+        argparser = ArgumentParser()
+
+        argparser.add_argument('--input_s3_path', type=str, help='Input Parquet Path', required=True)
+        argparser.add_argument('--output_s3_path', type=str, help='Output Parquet Path', required=True)
+
+        args = argparser.parse_args()
+
+        df = spark.read.parquet(args.input_s3_path)
+        df = df.repartition(20)
+
+        # Select only the fields that we want to put into Athena
+        athena_df = df.select(df.contigName.alias('chromosome'),
+                              'start',
+                              'end',
+                              df.variant.referenceAllele.alias('ref'),
+                              df.variant.alternateAllele.alias('alt'),
+                              'sampleId',
+                              'alleles')
+
+        athena_df = spark.createDataFrame(athena_df.rdd.flatMap(lambda x: convert_alleles_to_genotypes(x)))
+
+        athena_df.write.mode('overwrite').parquet(args.output_s3_path)
     finally:
         sc.stop()
 
