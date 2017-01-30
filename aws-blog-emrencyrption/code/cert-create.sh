e1031bcb6d1d414331e53ec6cf19bb0d5745f98e
Sriparasa <sriparas@c4b301b8aa4b.ant.amazon.com>
2017-01-30 18:10:55 -0500

    Added the shebang line and updated the subject to add wildcard

--- aws-blog-emrencyrption/code/cert-create.sh
+++ aws-blog-emrencyrption/code/cert-create.sh
@@ -1,3 +1,4 @@
-openssl req -newkey rsa:2048 -nodes -keyout privateKey.pem -x509 -days -30 -out certificateChain.pem
+#! /bin/bash
+openssl req -x509 -newkey rsa:1024 -keyout privateKey.pem -out certificateChain.pem -days 365 -nodes -subj '/C=US/S=Washington/L=Seattle/O=MyOrg/OU=MyDept/CN=*.ec2.internal'
 cp certificateChain.pem trustedCertificates.pem
 zip -r -X certs.zip privateKey.pem certificateChain.pem trustedCertificates.pem
